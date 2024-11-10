e82904ce27705d573ab5f7d3f28182766228629e
Cheikh Ly <lyibnoul2@gmail.com>
2024-11-10 16:28:23 +0100

    first commit

---
+++ c#2024/service/impl/clientServiceImpl.cs
@@ -0,0 +1,36 @@
+using Cours.Models;
+using Cours.Repository;
+namespace Cours.Service.Impl{
+    public class ClientServiceImpl : IClientService{
+        private readonly IClientRepository clientRepository;
+        public ClientServiceImpl(IClientRepository clientRepository){
+            this.clientRepository = clientRepository;
+        }
+       
+        public void Delete(int id){
+            clientRepository.Delete(id);
+        }
+     
+
+        List<Client> IClientService.FindAll()
+        {
+             return clientRepository.SelectAll();
+        }   
+        
+
+        Client IClientService.FindById(int id)
+        {
+             return clientRepository.SelectById(id);
+        }
+
+        public void Save(Client client)
+        {
+               clientRepository.Insert(client);
+        }
+
+        public void Update(Client client)
+        {
+            clientRepository.Update(client);
+        }
+    }
+}   
